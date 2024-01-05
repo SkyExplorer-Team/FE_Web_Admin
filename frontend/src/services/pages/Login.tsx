ad20f608511158dea4adbc4bfc2a29992b6c66b2
Nurul Mustofa <nioke8090@gmail.com>
2024-01-05 14:56:57 +0700

    temporary login style

---
+++ frontend/src/services/pages/Login.tsx
@@ -0,0 +1,71 @@
+import { useState } from "react";
+import { Container, Form, Button, Card } from "react-bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "../styles/LoginStyle.css";
+
+function Login() {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleLogin = () => {
+    console.log(`Login with username: ${username} and password: ${password}`);
+  };
+
+  return (
+    <div className="wrapper">
+      <Container>
+        <Card className="login-card">
+          <Card.Body>
+            <div className="row">
+              <div className="col firstColumnCardLogin">
+                <img
+                  src="https://i.ibb.co/xMPCMJZ/Frame-427320746.png"
+                  alt=""
+                  className="imageLoginFirstColumn img-fluid"
+                  style={{ backgroundSize: "cover", width: "100%" }}
+                />
+              </div>
+              <div className="col secondColumnCardLogin">
+                <Card.Title className="signInLabel">Sign In</Card.Title>
+                <Form className="login-form">
+                  <Form.Group controlId="formUsername">
+                    <Form.Label style={{ fontWeight: "bold" }}>
+                      Email
+                    </Form.Label>
+                    <Form.Control
+                      type="text"
+                      placeholder="Enter Your Email"
+                      value={username}
+                      onChange={(e) => setUsername(e.target.value)}
+                    />
+                  </Form.Group>
+
+                  <Form.Group controlId="formPassword">
+                    <Form.Label>Password</Form.Label>
+                    <Form.Control
+                      type="password"
+                      placeholder="Enter Your Password"
+                      value={password}
+                      onChange={(e) => setPassword(e.target.value)}
+                    />
+                  </Form.Group>
+
+                  <Button
+                    variant="primary"
+                    type="button"
+                    onClick={handleLogin}
+                    className="btn btn success mt-3"
+                  >
+                    Sign In
+                  </Button>
+                </Form>
+              </div>
+            </div>
+          </Card.Body>
+        </Card>
+      </Container>
+    </div>
+  );
+}
+
+export default Login;
