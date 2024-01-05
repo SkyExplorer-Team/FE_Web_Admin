041ab8707a98f056c1fa03d8a289bd8c2ef3127f
DwikiCahyo <70952085+DwikiCahyo@users.noreply.github.com>
2024-01-05 17:17:06 +0700

    feat : forgot password

--- frontend/src/services/App.tsx
+++ frontend/src/services/App.tsx
@@ -1,11 +1,13 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/login";
+import ForgotPassword from "./pages/ForgotPassword";
 
 const App = () => {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/forgot-password" element={<ForgotPassword />} />
       </Routes>
     </Router>
   );
