6d1152c923c59bf121c938b5f60357f6f831720a
Muhammad Bachtiar <muhammadbachtiar010902@gmail.com>
2024-01-06 14:05:49 +0700

    Fix : Login Page and Forget Password - Fix Login Page and Forget Password Design

--- frontend/src/services/pages/ForgotPassword.tsx
+++ frontend/src/services/pages/ForgotPassword.tsx
@@ -1,8 +1,13 @@
-import { Card, CardBody, Container, Form } from "react-bootstrap";
+import { useState } from "react";
+import { Card, Button, Container, Form } from "react-bootstrap";
 import "../styles/ForgotPasswordStyle.css";
-import SkyExploreLogo from "../../assets/sky-explre-logo.svg";
+import SkyExploreLogo from "../../assets/images/Brand_Logo.svg";
 
 export default function ForgotPassword() {
+  const [username, setUsername] = useState("");
+  const handleLogin = () => {
+    console.log(`username: ${username}`);
+  };
   return (
     <div className="wrapper">
       <Container>
@@ -18,25 +23,39 @@ export default function ForgotPassword() {
                 />
               </div>
               <div className="col body-col">
-                <div className="wrapper-logo">
-                  <img src={SkyExploreLogo} alt="" />
+                <div className="row">
+                  <div className="col-12 pb-3 text-end logoContainer" >
+                    <img src={SkyExploreLogo} alt="Logo" />
+                  </div>
+                  <div className="col-12 py-2 align-self-end">
+                    <Card.Title className="fw-semibold fs-2 mb-4">Forgot Password</Card.Title>
+                    <Card.Subtitle className="subtitle-card">
+                      Enter your email address below, and we'll send you a link to
+                      reset your password.
+                    </Card.Subtitle>
+                    <Form className="email-form">
+                      <Form.Group controlId="formUsername" className="mb-5">
+                        <label className="label-email">Email</label>
+                        <Form.Control
+                          type="email"
+                          placeholder="Enter Your Email"
+                          className="input-email mb-5"
+                          value={username}
+                          onChange={(e) => setUsername(e.target.value)}
+                        />
+                      </Form.Group>
+                      <div className="my-5"></div>
+                      <Button
+                          variant="primary"
+                          type="button"
+                          onClick={handleLogin}
+                          className="btn btn success mt-5"
+                        >
+                          Send Instruction
+                        </Button>
+                    </Form>
+                  </div>
                 </div>
-                <hr className="dashed" />
-                <Card.Title className="title-card">Forgot Password</Card.Title>
-                <Card.Subtitle className="subtitle-card">
-                  Enter your email address below, and we'll send you a link to
-                  reset your password.
-                </Card.Subtitle>
-                <Form className="email-form">
-                  <Form.Group controlId="formUsername">
-                    <label className="label-email">Email</label>
-                    <Form.Control
-                      type="text"
-                      placeholder="Enter Your Email"
-                      className="input-email"
-                    />
-                  </Form.Group>
-                </Form>
               </div>
             </div>
           </div>
--- frontend/src/services/pages/Login.tsx
+++ frontend/src/services/pages/Login.tsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { Container, Form, Button, Card } from "react-bootstrap";
+import {  Container, Form, Button, Card } from "react-bootstrap";
 import brandLogo from '..//../assets/images/Brand_Logo.svg';
 import "bootstrap/dist/css/bootstrap.min.css";
 import "../styles/LoginStyle.css";
@@ -8,7 +8,11 @@ import { Link } from "react-router-dom";
 function Login() {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
+  const [isPasswordVisible, setPasswordVisible] = useState(false);
 
+  const togglePasswordVisibility = () => {
+    setPasswordVisible(!isPasswordVisible);
+  };
   const handleLogin = () => {
     console.log(`Login with username: ${username} and password: ${password}`);
   };
@@ -29,7 +33,7 @@ function Login() {
               </div>
               <div className="col secondColumnCardLogin">
                   <div className="row">
-                    <div className="col-12 py-3 text-end logoContainer" >
+                    <div className="col-12 pb-3 text-end logoContainer" >
                     <img
                         src={brandLogo}
                         alt="Logo"
@@ -37,8 +41,8 @@ function Login() {
                         style={{ backgroundSize: "cover", width: "116px", height: "24px"}}
                       />
                     </div>
-                    <div className="col-12 py-3">
-                    <Card.Title className="signInLabel fw-semibold fs-3">Sign In</Card.Title>
+                    <div className="col-12 py-2 align-self-end">
+                    <Card.Title className="fw-semibold fs-2">Sign In</Card.Title>
                       <Form className="login-form">
                         <Form.Group controlId="formUsername" className="py-2">
                           <Form.Label>
@@ -52,22 +56,29 @@ function Login() {
                             onChange={(e) => setUsername(e.target.value)}
                           />
                         </Form.Group>
-                        <Form.Group controlId="formPassword" className="py-2">
+                        <Form.Group controlId="formPassword" className="passwordInput py-2">
                           <Form.Label>Password</Form.Label>
-                          <Form.Control
-                            className="my-1"
-                            type="password"
-                            placeholder="Enter Your Password"
-                            value={password}
-                            onChange={(e) => setPassword(e.target.value)}
-                          />
+                          <div className="input-group">
+                            <Form.Control
+                              className="my-1"
+                              type={isPasswordVisible ? 'text' : 'password'}
+                              placeholder="Enter Your Password"
+                              value={password}
+                              onChange={(e) => setPassword(e.target.value)}
+                            />
+                             <div className="input-group-append my-1">
+                              <span onClick={togglePasswordVisibility}>
+                                {isPasswordVisible ? <i className="fa-solid fa-eye-slash"></i> : <i className="fa-solid fa-eye"></i>}
+                              </span>
+                            </div>
+                          </div>
                         </Form.Group>
-                        <div className="forgetPassword py-3"><Link to="/forgot-password">Forgot Password?</Link></div>
+                        <div className="forgetPassword pt-2 pb-4"><Link to="/forgot-password">Forgot Password?</Link></div>
                         <Button
                           variant="primary"
                           type="button"
                           onClick={handleLogin}
-                          className="btn btn success mt-3 my-3"
+                          className="btn btn success"
                         >
                           Sign In
                         </Button>
